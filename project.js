5d8fcffa05d21b5fab0861f81de6652c299c34a0
John Ro <67990368+jmr54@users.noreply.github.com>
2023-02-10 18:39:21 -0800

    Add files via upload

--- project.js
+++ project.js
@@ -1,9 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.use(express.json())
+const patient = require('./routes/patient');
+app.use('/patient', patient);
 
-const patient = require('./routes/patient')
-app.use('/patient', patient)
-
-app.listen(3000, ()=> console.log('Server working'))
+app.listen(3000, ()=> console.log('Server working'));
